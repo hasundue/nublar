11359e9de65a4f833c26bfd85565dead668f76e3
Shun Ueda <hasundue@gmail.com>
2022-09-28 08:55:28 +0900

    test: add test for help

--- test.ts
+++ test.ts
@@ -1,5 +1,7 @@
-import { assert } from "https://deno.land/std@0.157.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.157.0/testing/asserts.ts";
+import $ from "https://deno.land/x/dax@0.12.0/mod.ts";
 
-Deno.test("test", () => {
-  assert(true);
+Deno.test("help", async () => {
+  const result = await $`deno task run --help`;
+  assertEquals(result.code, 0);
 });
