aaac02eef1ede58ad71ed2db928b7dca40d81b90
Shun Ueda <hasundue@gmail.com>
2022-09-28 23:05:11 +0900

    test: add real tests for list and update subcommands

--- test.ts
+++ test.ts
@@ -1,22 +1,52 @@
-import { assertEquals } from "https://deno.land/std@0.157.0/testing/asserts.ts";
+import {
+  assertMatch,
+  assertNotMatch,
+} from "https://deno.land/std@0.157.0/testing/asserts.ts";
 import $ from "https://deno.land/x/dax@0.12.0/mod.ts";
 
-Deno.test("help", async () => {
-  const result = await $`deno task run --help`;
-  assertEquals(result.code, 0);
+const cwd = Deno.cwd();
+
+async function installScripts(specs: [name: string, url: string][]) {
+  for (const spec of specs) {
+    await $`deno install -A --root . --name ${spec[0]} ${spec[1]}`.text();
+  }
+}
+
+async function createTestEnv() {
+  await installScripts([
+    ["nublar", "https://deno.land/x/nublar@0.1.2/nublar.ts"],
+    ["udd", "https://deno.land/x/udd@0.5.0/main.ts"],
+  ]);
+  await $`touch bin/deno`;
+}
+
+function withTestEnv(
+  name: string,
+  fn: () => void | Promise<void>,
+) {
+  Deno.test(name, async () => {
+    const tempDir = Deno.makeTempDirSync();
+    try {
+      Deno.chdir(tempDir);
+      await createTestEnv();
+      await fn();
+    } finally {
+      Deno.chdir(cwd);
+      Deno.removeSync(tempDir, { recursive: true });
+    }
+  });
+}
+
+withTestEnv("list", async () => {
+  const result = await $`bin/nublar list --root .`.text();
+  assertMatch(result, /nublar/);
+  assertMatch(result, /udd/);
+  assertNotMatch(result, /deno/);
 });
 
-Deno.test("version", async () => {
-  const result = await $`deno task run --version`;
-  assertEquals(result.code, 0);
-});
-
-Deno.test("list", async () => {
-  const result = await $`deno task run list`;
-  assertEquals(result.code, 0);
-});
-
-Deno.test("update", async () => {
-  const result = await $`deno task run update`;
-  assertEquals(result.code, 0);
+withTestEnv("update", async () => {
+  const result = await $`bin/nublar update --root .`.text();
+  assertNotMatch(result, /nublar/);
+  assertMatch(result, /udd/);
+  assertNotMatch(result, /deno/);
 });
