defb934ae521a11e2789c99131f5ae0d531fe513
denopendabot <denopendabot@gmail.com>
2022-10-17 09:41:41 +0900

    build(deps): bump deno.land/x/dax@0.14.0/mod.ts to 0.14.1

--- test.ts
+++ test.ts
@@ -3,7 +3,7 @@ import {
   assertMatch,
   assertNotMatch,
 } from "https://deno.land/std@0.159.0/testing/asserts.ts";
-import { $, CommandBuilder } from "https://deno.land/x/dax@0.14.0/mod.ts";
+import { $, CommandBuilder } from "https://deno.land/x/dax@0.14.1/mod.ts";
 
 const isWindows = Deno.build.os === "windows";
 const cwd = Deno.cwd();
