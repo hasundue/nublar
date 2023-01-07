9607d82c0b8f4efb43a0aa018aeaa0c31a60e894
denopendabot <denopendabot@github.com>
2023-01-07 00:13:02 +0000

    build(deps): bump std from 0.170.0 to 0.171.0

--- nublar.ts
+++ nublar.ts
@@ -1,5 +1,5 @@
-import { join, resolve } from "https://deno.land/std@0.170.0/path/mod.ts";
-import { ensureDirSync } from "https://deno.land/std@0.170.0/fs/mod.ts";
+import { join, resolve } from "https://deno.land/std@0.171.0/path/mod.ts";
+import { ensureDirSync } from "https://deno.land/std@0.171.0/fs/mod.ts";
 import { Command } from "https://deno.land/x/cliffy@v0.25.7/command/mod.ts";
 import { Table } from "https://deno.land/x/cliffy@v0.25.7/table/mod.ts";
 import dir from "https://deno.land/x/dir@1.5.1/mod.ts";
