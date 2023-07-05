3fc632f3bedb76fe90e031c53d8122761b98cbfd
denopendabot <denopendabot@github.com>
2023-07-05 00:16:24 +0000

    build(deps): bump dax from 0.32.0 to 0.33.0

--- test.ts
+++ test.ts
@@ -3,7 +3,7 @@ import {
   assertMatch,
   assertNotMatch,
 } from "https://deno.land/std@0.193.0/testing/asserts.ts";
-import { $, CommandBuilder } from "https://deno.land/x/dax@0.32.0/mod.ts";
+import { $, CommandBuilder } from "https://deno.land/x/dax@0.33.0/mod.ts";
 
 const isWindows = Deno.build.os === "windows";
 const cwd = Deno.cwd();
